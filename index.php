36040b28be005083085dcb8c14f2ec31c67dc4a2
Jesus Fernandez <jesufernandez@csumb.edu>
2018-08-28 05:53:48 +0000

    updating index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Jesus Fernandez';
+echo 'Jesus Fernandez cst336';
 
 ?>
 </body>
