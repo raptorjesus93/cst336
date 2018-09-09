4ff54242d2b68eeff8cb3e549961b449ed2f293f
Jesus Fernandez <jesufernandez@csumb.edu>
2018-09-09 20:25:54 +0000

    adding files

---
+++ Labs/lab2/777/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+        <link rel="stylesheet" href="css/styles.css" type="text/css" />
+    </head>
+    <body>
+        <div id="main">
+        <?php
+            include 'inc/functions.php';
+            play();
+        ?>
+        <form>
+            <input type="submit" value="Spin!"/>
+        </form>
+        </div>
+    </body>
+</html>
