c5f377b070383f215005f8e70bb20b75101b6889
Jesus Fernandez <jesufernandez@csumb.edu>
2018-09-26 06:09:57 +0000

    added the footer

--- hw/hw2/index.php
+++ hw/hw2/index.php
@@ -79,5 +79,18 @@
 			}
 		?>
 	</form>
+    <footer>
+        
+        <hr>
+        <span>cst 336, 2018&copy; Fernandez <br>
+        <strong>Disclaimer:</strong> The information in this website is fictitous. <br>
+        It is used for academic purposes only.</span>
+        <br>
+            
+        <img src="../../img/csumb_logo.png" alt="CSUMB logo">
+        <img src="../../img/buddy_verified.png" alt="buddy badge">
+        
+    </footer>
+
 </body>
 </html>
