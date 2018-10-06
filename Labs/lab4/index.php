98ffb12cbfc3ad34ab0e5f8ec90a9c901c1ee4cf
Jesus Fernandez <jesufernandez@csumb.edu>
2018-10-06 17:42:56 +0000

    added buddy badge

--- Labs/lab4/index.php
+++ Labs/lab4/index.php
@@ -106,6 +106,19 @@
         }
         ?>
         
+        <footer>
+            <hr>
+             
+            <span>cst 336, 2018&copy; Fernandez <br>
+            <strong>Disclaimer:</strong> The information in this website is fictitous. <br>
+            It is used for academic purposes only.</span>
+            <br>
+            
+            <img src="../../img/csumb_logo.png" alt="CSUMB logo">
+            <img src="../../img/buddy_verified.png" alt="buddy badge">
+        
+        </footer>
+        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     </body>
